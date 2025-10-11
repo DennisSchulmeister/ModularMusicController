7eeb5808db72a34585c357107ad3c3fd478b336f
Dennis Schulmeister-Zimolong <dennis@patk.org>
2025-10-11 16:28:19 +0200

    Webconfig: HTMX page loads

--- Webconfig/browser/index.ts
+++ Webconfig/browser/index.ts
@@ -11,6 +11,38 @@ import "alpinejs";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
-import "htmx.org";
+import htmx from "htmx.org";
 
 import "./style.css";
+
+// Extend the Window interface to include 'htmx'
+declare global {
+	interface Window {
+		htmx: typeof htmx;
+	}
+}
+
+window.htmx = htmx;
+
+// Initialize page
+window.addEventListener("DOMContentLoaded", () => {
+    // Toggle active navbar item upon navigation
+    for (let navLink of document.querySelectorAll("header .nav-link")) {
+        navLink.addEventListener("click", (event: Event) => {
+            for (let navLink of document.querySelectorAll("header .nav-link")) {
+                navLink.classList.remove("active");
+                navLink.removeAttribute("aria-current");
+            }
+
+            if (!event.target) return;
+            let target: HTMLElement = event.target as HTMLElement;
+            target.classList.add("active");
+            target.setAttribute("aria-current", "page");
+        });
+    }
+
+    // Switch to first page on page load
+    let firstNavLink = document.querySelector("header .nav-item:first-child a");
+    if (firstNavLink) (firstNavLink as HTMLElement).click();
+});
+
